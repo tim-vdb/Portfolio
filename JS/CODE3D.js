49994cbec3caccd6bfa7a4e6d4d71c6cb57d409c
Cassetete <timotheevdbosch@gmail.com>
2024-10-07 12:07:40 +0200

    test 11 threeJS

--- JS/CODE3D.js
+++ JS/CODE3D.js
@@ -1,4 +1,4 @@
-import * as THREE from './three/build/three.module.js';
+import * as THREE from "../node_modules/three/build/three.module.js";
 import { GLTFLoader } from './three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from './three/examples/jsm/controls/OrbitControls.js';
 import { EffectComposer } from './three/examples/jsm/postprocessing/EffectComposer.js';
